5ec40df57744249c66323dee23e7f9bf23a1f8a5
Deliaz <D3liaz@gmail.com>
2017-03-16 10:09:24 +0700

    Allow to set device address and port via options

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ const chalk = require('chalk');
 
 program
     .version(version)
-    .arguments('<url>')
-    .option('-v, --verbose', 'enable verbose mode') // TODO
+    .usage('<url> [options]')
+    // .option('-v, --verbose', 'enable verbose mode') // TODO verbose output
+    .option('-d, --device <device>', 'hostname or IP', 'apple-tv.local')
+    .option('-p, --port <port>', 'port number', 7000)
     .parse(process.argv);
 
-const url = program.args[0]; //TODO pattern to check if it is a youtube link
+const url = program.args[0]; //TODO pattern to check youtube link
 if (!url) {
     program.help();
 }
-
 
 let logGetInfo = ora({
     text: 'Loading video info...',
@@ -53,7 +54,8 @@ ytdl
     .then(playVideo)
     .catch(err => {
         if (err) {
-            console.error(chalk.red('Error'), err);
+            console.error('\n' + chalk.red('Error'), err.message);
+            // TODO Show stack in verbose
         }
         process.exit(-1);
     });
@@ -89,13 +91,15 @@ function playVideo({url, title}) {
     return new Promise((resolve, reject) => {
         logConnecting.start();
 
-        const airplayDevice = new AirPlay('apple-tv.local'); // TODO not only default
+        const airplayDevice = new AirPlay(program.device, program.port); // TODO not only default
 
         if (!airplayDevice) {
             logConnecting.fail('AirPlay device connection error.');
             reject();
             return;
         }
+
+        //TODO airplayDevice.on error
 
         airplayDevice.play(url, function (err) {
             if (err) {
@@ -104,8 +108,7 @@ function playVideo({url, title}) {
                 return;
             }
 
-            logConnecting.succeed(`Connected to ${chalk.blue('apple-tv.local')}`);
-
+            logConnecting.succeed(`Connected to ${chalk.blue(program.device + ':' + program.port)}`);
             logPlay.text = `Playing "${chalk.green(title)}". Press ${chalk.yellow('Ctrl+C')} to stop.`;
             logPlay.start();
         })
